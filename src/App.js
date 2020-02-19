8ea8b9cbf766478bc7456117cbeca05bb5efa396
Bram Beirens <bram.beirens@be.ibm.com>
2020-02-19 16:01:25 +0100

    catch fetch + upgrade packages

--- src/App.js
+++ src/App.js
@@ -19,7 +19,8 @@ class App extends Component {
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({monsters: users}));
+      .then(users => this.setState({monsters: users}))
+      .catch(err => console.error(err));
   }
 
   /*handleChange(e) {
