7949d72cb11e83617e4f25e38e485c593c790d72
Bram Beirens <bram.beirens@be.ibm.com>
2020-02-19 14:54:58 +0100

    Updates

---
+++ precache-manifest.0c249577589327bf184d1a3ac3e00644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a874667a6d5d8995199165b67320eaf3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d0f9ce79f55c4d6e0632",
+    "url": "/monsters-rolodex/static/css/main.2d5ef471.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0f9ce79f55c4d6e0632",
+    "url": "/monsters-rolodex/static/js/main.e468a9e3.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
